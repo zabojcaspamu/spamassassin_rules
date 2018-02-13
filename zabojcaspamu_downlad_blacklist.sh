d2b6b2aa0ac876b8bbc2cdede55c0037e9b72472
zabojcaspamu <zabojcaspamu@gmail.com>
2018-02-13 19:24:01 +0100

    Skrypt sciagajacy poprawki

--- zabojcaspamu_downlad_blacklist.sh
+++ zabojcaspamu_downlad_blacklist.sh
@@ -7,7 +7,7 @@
 WORK_DIR="/tmp"
 # Katalog docelowy z ktorego zaczytuje reguly SA
 SAVE_DIR="/etc/spamassassin"
-# Log z dzialanai skryptu
+# Log z dzialania skryptu
 LOG_FILE="/var/log/zabojcaspamu.log"
 
 #
@@ -17,24 +17,24 @@ FILE_BL=(local.cf.BL.ZABOJCASPAMU local.cf.BL.SPAMTRAP)
 
 for FILE in ${FILE_BL[*]};do
     SIGN_DATA="[$(date +%Y-%m-%d_%H:%m:%S)]"
-    curl -s https://zabojcaspamu.pl/$FILE_BL -o ${WORK_DIR}/${FILE_BL}
+    curl -s https://zabojcaspamu.pl/$FILE -o ${WORK_DIR}/${FILE}
     RETV=$?
     if [ "$RETV" -ne "0" ];then
        echo "$SIGN_DATA Blad podczas sciagania pliku" >> ${LOG_FILE}
        exit 1
     fi
-    SUMA=`curl -s https://zabojcaspamu.pl/md5sum.txt|grep  $FILE_BL|awk '{print $1}'`
-    echo "$SUMA ${WORK_DIR}/${FILE_BL}" | md5sum -c - >/dev/null
+    SUMA=`curl -s https://zabojcaspamu.pl/md5sum.txt|grep  $FILE|awk '{print $1}'`
+    echo "$SUMA ${WORK_DIR}/${FILE}" | md5sum -c - >/dev/null
     RETV=$?
     if [ "$RETV" -ne "0" ];then
-       echo "$SIGN_DATA Plik $PLIK sciagniety ale suma kontrola sie nie zgadza"  >> ${LOG_FILE}
+       echo "$SIGN_DATA Plik $FILE sciagniety ale suma kontrola sie nie zgadza"  >> ${LOG_FILE}
        exit 1
     fi
 
-    cp -f ${WORK_DIR}/${FILE_BL} ${SAVE_DIR}/${FILE_BL} 2>/dev/null
+    cp -f ${WORK_DIR}/${FILE} ${SAVE_DIR}/${FILE} 2>/dev/null
     RETV=$?
     if [ "$RETV" -ne "0" ];then
-       echo "$SIGN_DATA Plik $PLIK sciagniety suma kontrola OK ale przegranie do $SAVE_DIR katalogu sie nie powiodlo"  >> ${LOG_FILE}
+       echo "$SIGN_DATA Plik $FILE sciagniety suma kontrola OK ale przegranie do $SAVE_DIR katalogu sie nie powiodlo"  >> ${LOG_FILE}
        exit 1
     fi
 
